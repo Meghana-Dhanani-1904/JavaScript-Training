3793ba0a09c298243093a7b60408686c3a800d4a
Meghana Dhanani <136313785+Meghanadhanani-1904@users.noreply.github.com>
2023-06-23 15:56:31 +0530

    Add files via upload

---
+++ string_questons.js
@@ -0,0 +1,188 @@
+//  const sentence = "JavaScript is amazing!";
+// console.log(sentence.lastIndexOf("a", 12));//doubt
+
+//  const sentence = "JavaScript is flexible!";
+// console.log(sentence.charAt(sentence.length - 1));//doubt
+
+//  const sentence = "JavaScript is versatile!";
+// console.log(sentence.split(" ").reverse().join(" "));//doubt
+
+
+// Which string method is used to determine the length of a string?//length
+
+// How would you convert a string to uppercase using a string method?//toUppercase
+
+// Which method is used to find the position of a specified value within a string?//indexof
+// How would you concatenate two strings using a string method?//with the help of "concate" sting method
+// Which method would you use to extract a specific portion of a string?//substring
+
+// How can you replace a specific value with another value within a string using a method?//replace
+// Which method would you use to split a string into an array of substrings based on a specified separator?//.substring().split()
+// How would you remove leading and trailing whitespace from a string using a method?//trim
+// Which method is used to search for a specified pattern and return the first occurrence in a string?//substr
+// How can you check if a string starts with a specific value using a string method?//startWith
+
+
+// // const message = "Hello, World!";
+// // console.log(message.length);
+
+// const str = "HelloWorld";
+// console.log(str.toLowerCase());//helloworld
+
+// const sentence = "I love JavaScript!";
+// console.log(sentence.indexOf("JavaScript"));//7 it will only give first letter's index
+
+// const name = "John";
+// const greeting = `Hello, ${name}!`;
+// console.log(greeting);//Hello, John!
+
+// // const text = "Hello, World!";
+// // console.log(text.substring(7));//World! beecause we haven't provide lenth to the string
+
+// const message = "Hello, World!";
+// console.log(message.replace("World", "Universe"));//"Hello, Universe!"
+
+// const words = "apple, banana, orange";
+// const fruits = words.split(",");
+// console.log(fruits);//[ 'apple', ' banana', ' orange' ]
+
+// const text = "   Hello,World!   ";
+// console.log(text.trim());//"Hello,World!"
+
+// const sentence = "The quick brown fox jumps over the lazy dog.";
+// console.log(sentence.includes("dog"));//True
+
+// const str = "Hello, World!";
+// console.log(str.endsWith("!"));//true
+
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.charAt(4));//S
+
+// const str1 = "Hello";
+// const str2 = "World";
+// console.log(str1.concat(", ", str2));//hello, World
+
+// const sentence = "JavaScript is awesome!";
+// console.log(sentence.slice(0, 10));//JavaScript 
+
+// const text = "Hello, World!";
+// console.log(text.repeat(3));//Hello, World!Hello, World!Hello, World!
+
+// const sentence = "The quick brown fox";
+// console.log(sentence.split(" "));//
+
+// const str = "Hello, World!";
+// console.log(str.indexOf("o"));//4
+
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.substring(4, 10));//Script
+
+// const text = "Hello, World!";
+// console.log(text.charAt(7));//W
+
+// const sentence = "I love JavaScript!";
+// console.log(sentence.split("o"));//"I l","ve JavaScript!"
+
+// const str = "Hello, World!";
+// console.log(str.includes("World"));//true
+
+// const sentence = "JavaScript is cool!";
+// console.log(sentence.lastIndexOf("o"));//16
+
+// const text = "Hello, World!";
+// console.log(text.startsWith("Hello"));//true
+
+// const str = "Hello, World!";
+// console.log(str.toUpperCase());//HELLO, WORLD!
+
+// const sentence = "JavaScript is awesome!";
+// console.log(sentence.toLowerCase());//javascript is awesome!
+
+// const str = "Hello, World!";
+// console.log(str.charAt(13));// empty
+
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.substr(4, 10));//Script is
+
+// const text = "Hello, World!";
+// console.log(text.includes("WORLD"));//false
+
+// const sentence = "JavaScript is amazing!";
+// console.log(sentence.replace("amazing", "awesome"));//JavaScript is awesome!
+
+// const str = "Hello, World!";
+// console.log(str.charCodeAt(4));//111
+
+// const sentence = "JavaScript is powerful!";
+// console.log(sentence.split(" ").length);//'JavaScript','is','powerful!' 
+
+//  const text = "Hello, World!";
+// console.log(text.search("W"));//7
+
+//  const sentence = "JavaScript is dynamic!";
+//  console.log(sentence.slice(-7));//ynamic!
+
+// const str = "Hello, World!";
+// console.log(str.concat(" Goodbye!"));//Hello, World! Goodbye!
+
+
+// const sentence = "JavaScript is interesting!";
+// console.log(sentence.substring(10));// is interesting!
+
+//  const str = "Hello, World!";
+// console.log(str.search(""));//
+
+//  const sentence = "JavaScript is powerful!";
+// console.log(sentence.substring(4, 10));//Script
+
+//  const str = "Hello, World!";
+// console.log(str.charCodeAt(0));//72
+
+//  const sentence = "JavaScript is amazing!";
+// console.log(sentence.slice(4, -10));//Script i
+
+//  const text = "Hello, World!";
+// console.log(text.indexOf("o", 5));//8
+
+//  const sentence = "JavaScript is fun!";
+// console.log(sentence.startsWith("Java"));//true
+
+//  const str = "Hello, World!";
+// console.log(str.endsWith("!"));//true
+
+//  const text = "Hello,World!";
+// console.log(text.substring(7, 2));//llo,W
+
+//  const sentence = "JavaScript is cool!";
+// console.log(sentence.concat(" I love it!"));//JavaScript is cool! I love it!
+
+//  const str = "Hello, World!";
+// console.log(str.toUpperCase().charAt(8));//O
+
+//  const str = "Hello, World!";
+// console.log(str.toUpperCase().charAt(7));//W
+
+// const sentence = "JavaScript is amazing!";
+// console.log(sentence.replace("amazing", "awesome").toUpperCase());//JAVASCRIPT IS AWESOME!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
