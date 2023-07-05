376f16afad8d704d5d43d138807c607ad68e9b3b
Meghana Dhanani <136313785+Meghanadhanani-1904@users.noreply.github.com>
2023-07-05 16:12:49 +0530

    Update array Question.js

--- array Question.js
+++ array Question.js
@@ -408,3 +408,27 @@
 // console.log(result);
 
 
+// 12th Reduce Method
+
+// const products = [
+//   { name: "Product 1", price: 20, category: "Electronics" },
+//   { name: "Product 2", price: 30, category: "Clothes" },
+//   { name: "Product 3", price: 40, category: "Electronics" },
+//   { name: "Product 4", price: 50, category: "Clothes" },
+//   { name: "Product 5", price: 60, category: "Clothes" },
+//   { name: "Product 6", price: 70, category: "Electronics" },
+//   { name: "Product 7", price: 80, category: "Clothes" },
+//   { name: "Product 8", price: 90, category: "Electronics" },
+//   { name: "Product 8", price: 90, category: "Electro" },
+// ];
+
+// let result = products.reduce(function (acc, curr) {
+//   if (acc[curr.category]) {
+//     acc[curr.category] = ++acc[curr.category];
+//   } else {
+//     acc[curr.category] = 1;
+//   }
+//   return acc;
+// }, {});
+// console.log(result);
+
