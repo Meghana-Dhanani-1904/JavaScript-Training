f3da4d38a5182957c4a53ee945a6dcf6df18232b
Meghana Dhanani <136313785+Meghanadhanani-1904@users.noreply.github.com>
2023-07-13 10:54:37 +0530

    Add files via upload
    Commited before Session

---
+++ date Methods(13-07).js
@@ -0,0 +1,13 @@
+// const date = new Date(); // Current date and time
+// date.setDate(date.getDate() + 2);
+
+// console.log(date);
+
+// function formet(transform) {
+//   var a = new Date("2023-03-04");
+//   a.setDate(2 + a.getDate());
+//   final_date = a.getDate() + "/" + (a.getMonth() + 1) + "/" + a.getFullYear();
+//   return final_date;
+// }
+
+// console.log(formet("dd/mm/yyyy"));
